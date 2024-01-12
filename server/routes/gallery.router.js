800fb172ef6a28b9889a52854ac741289807e05b
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-01-12 17:57:05 -0600

    router.put created and tested with queryParams

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,14 +3,29 @@ const pool = require('../modules/pool.js')
 const router = express.Router();
 
 // PUT /gallery/like/:id
-router.put('like/:id', (req, res) => {
-  // code here
+router.put('/like/:id', (req, res) => {
+  // declare queryText which will UPDATE likes by id by +1
+  const queryParams = req.params.id
+  const queryText = `UPDATE "gallery" SET "likes" = "likes" + 1 WHERE id=$1`
+
+  pool
+    .query(queryText, [queryParams])
+  .then((result) => {
+    console.log("Update Successful");
+    res.sendStatus(201)
+  })
+  .catch((err) => {
+    console.log("Error Updating:", err);
+    res.sendStatus(500)
+  })
+
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // declare queryText which will SELECT all from database to return to DOM
   const queryText = `SELECT * FROM gallery;`
+  
   // send query w/ pool
   pool
     .query(queryText)
