8a6321e9a6dcf3ccea59597a4d5a0bbfef57bcc1
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-01-12 17:24:24 -0600

    created router.get and tested w/ postman

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const pool = require('../modules/pool.js') 
 const router = express.Router();
 
 // PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
+router.put('like/:id', (req, res) => {
   // code here
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  // declare queryText which will SELECT all from database to return to DOM
+  const queryText = `SELECT * FROM gallery;`
+  // send query w/ pool
+  pool
+    .query(queryText)
+  // THEN res.send result back to client in result.rows
+    .then((result) => {
+      console.log("Result:", result);
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      alert('Error getting gallery:', err)
+      res.sendStatus(500)
+    })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 
 function App() {
     return (
-      <div>
+      <div data-testid="app">
         <header>
           <h1>React Gallery</h1>
         </header>
 
         <p>The gallery goes here!</p>
+        <img src="images/elk_square.jpeg"/>
         <img src="images/goat_small.jpg"/>
+        <img src="images/mule_deer.jpeg"/>
       </div>
     );
 }
